be94e1791a0284e4797e73270d8b3efceb5d5b52
Mauricio Cantor <topomajor1@hotmail.com>
2014-01-29 14:16:06 -0500

    Actualizar About

--- pages/about.php
+++ pages/about.php
@@ -22,10 +22,10 @@
   <!-- end one-third column ends here -->
   <div class="one_third">
     <section class="aboutonecenter">
-      <h2>Our Motivation</h2>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-      <img class="raster" src="images/raster.png" alt=""/>
-      <p class="quote">Etiam eget mi enim, non ultricies nisi voluptatem, illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.</p>
+      <h2>Nuestra Motivación</h2>
+      <p>Nuestra motivación es generada por las necesidades de las empresas, para recibir productos de calidad, en lo que respecta a la prestación de servicios orientados a la creación, mantenimiento y renovación de sus sitios web, de forma que se pueda entregar un completo servicio de hosting, almacenamiento de bases de datos, programación de aplicativos web y soporte técnico para equipos informáticos, con costos asequibles, resultados óptimos y de calidad, acordes a los requerimientos y solicitudes de nuestros clientes.</p>
+      <!-- <img class="raster" src="../images/raster.png" alt=""/>
+      <p class="quote">Etiam eget mi enim, non ultricies nisi voluptatem, illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.</p> -->
     </section>
   </div>
   <!-- end one-third column ends here -->
